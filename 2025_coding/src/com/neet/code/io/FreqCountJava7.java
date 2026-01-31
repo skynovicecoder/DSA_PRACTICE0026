3b22ab647e73c7e880d7199d3e2b3d0498e5bfb7
Sonil Kumar <sonilcs0087@gmail.com>
2026-02-01 00:08:19 +0530

    Enhance frequency count with Java 8 Collectors
    Added Java 8 implementation for counting frequency of elements using Collectors.

--- 2025_coding/src/com/neet/code/io/FreqCountJava7.java
+++ 2025_coding/src/com/neet/code/io/FreqCountJava7.java
@@ -7,9 +7,10 @@ import java.util.Map;
 public class FreqCountJava7 {
 	public static void main(String args[]) {
 
-		// i
+		// input
 		int arr[] = { 1, 2, 1, 3, 4, 5, 6, 6, 7, 7, 1 };
 
+		//using java 7
 		Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
 
 		for (int i = 0; i < arr.length; i++) {
@@ -24,6 +25,14 @@ public class FreqCountJava7 {
 		}
 		System.out.print(mp);
 
+		//Using Java 8, just for remebering usage of Collectors
+		Map<Integer, Long> freqMap = Arrays.stream(arr)
+                                           .boxed()
+                                           .collect(Collectors.groupingBy(n -> n, Collectors.counting()));
+
+        System.out.println("Frequency of elements: " + freqMap);
+
 	}
 
 }
+
