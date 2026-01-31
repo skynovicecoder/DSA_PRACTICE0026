c2466de516019607ffd982105638bd7c2f9ff721
Sonil Kumar <sonilcs0087@gmail.com>
2026-01-31 23:49:52 +0530

    Refactor AddDigits class and improve addDigits method
    Refactor addDigits method to handle edge case for zero and simplify return statement.

--- 2025_coding/src/com/neet/code/io/AddDigits.java
+++ 2025_coding/src/com/neet/code/io/AddDigits.java
@@ -3,22 +3,16 @@ package com.neet.code.io;
 public class AddDigits {
 
 	public int addDigits(int num) {
-
-		int res = num - 1;
-
-		int data = (res%9);
-
-		System.out.print(data + 1);
-
-		return 0;
+		//digitalRoot(n)=1+(n−1)%9
+		if (num == 0) return 0;  //edge case was missing, your code will throw error for zero
+        return 1 + (num - 1) % 9; //why to create intermediate variable unecessary when you can do in one go
 	}
 
 	public static void main(String args[]) {
-		
+		int n = 38;
 		AddDigits addDigits=new AddDigits();
-		
-		addDigits.addDigits(38);
-
+		System.out.println("Digital root of " + n + " is: " + addDigits.addDigits(n));
 	}
 
 }
+
